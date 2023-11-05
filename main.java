3057f62bd68a0e21b95fba7008f6016cead675f4
Serkan Ağdaş <76965279+serkanagdas@users.noreply.github.com>
2023-11-05 22:06:15 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[]args){
+
+        String userName, password;
+        int degisim;
+
+        Scanner inp= new Scanner(System.in);
+
+        System.out.print("Kullanıcı Adınız: ");
+        userName = inp.nextLine();
+        System.out.print("Şifreniz: ");
+        password = inp.nextLine();
+
+        if (userName.equals("serkanagdas")){
+            if (password.equals("121212serkan")) {
+                System.out.print("Sisteme hoşgeldiniz");
+            }else
+                System.out.print("Şifrenizi hatalı değiştirmek ister misiniz?\n" +
+                        "Evet ise 1\n" +
+                        "Hayır ise 2");
+            degisim = inp.nextInt();
+
+            switch (degisim){
+
+                case 1 :
+                    System.out.print("Lütfen yeni şifrenizi giriniz =");
+                    inp.nextLine();
+                    password = inp.nextLine();
+                    if (password.equals("Esatgs123#")){
+                        System.out.print("Şifre oluşturulamadı, lütfen başka şifre giriniz");
+                    }else
+                        System.out.println("Şifre oluşturuldu");
+                    break;
+                case 2 :
+                    System.out.print("İyi günler dileriz.");
+                    break;
+            }
+
+
+        }else
+            System.out.print("Lütfen kullanıcı adınızı kontrol ediniz.");
+
+
+
+
+
+    }
+}
